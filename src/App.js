4674e992260509d2ba08f3a90f435b583a54b982
Franco <franco.hirschfeldt@mail.com>
2021-09-30 14:10:30 -0300

    Creacion de componentes utilizando props.
    Ademas sume Sass

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Components/navBar/navBar";
-import "./App.css";
+import ItemListContainer from "./Components/ItemListContainer/itemListContainer";
+import "./Css/App.css";
 
 class App extends React.Component {
   render() {
     return (
       <div className="grilla">
         <Navbar />
+        <ItemListContainer class="contenedor" greeting="Que onda anaconda" />
       </div>
     );
   }
--- src/Components/navBar/navBar.js
+++ src/Components/navBar/navBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import CartButton from "./CartButton/cartButton";
 
 const NavBar = () => {
   return (
@@ -7,31 +8,32 @@ const NavBar = () => {
         <h5>Muttuals</h5>
       </div>
       <li className="navItem">
-        <a className="navButton" href="#">
+        <a className="navButton" href="Index">
           <img src="./Multimedia/icons8-house-64.png" alt="Index" />
         </a>
       </li>
       <li className="navItem">
         \
-        <a className="navButton" href="#">
+        <a className="navButton" href="Portfolio">
           <img src="Multimedia/icons8-web-design-64.png" alt="Portfolio" />
         </a>
       </li>
       <li className="navItem">
-        <a className="navButton" href="#">
+        <a className="navButton" href="Nosotras">
           <img src="Multimedia/icons8-test-account-64.png" alt="Nosotras" />
         </a>
       </li>
       <li className="navItem">
-        <a className="navButton" href="#">
+        <a className="navButton" href="Servicios">
           <img src="Multimedia/icons8-checkout-64.png" alt="Servicios" />
         </a>
       </li>
       <li className="navItem">
-        <a className="navButton" href="#">
+        <a className="navButton" href="Contacto">
           <img src="Multimedia/icons8-contact-64.png" alt="Contacto" />
         </a>
       </li>
+      <CartButton />
     </nav>
   );
 };
