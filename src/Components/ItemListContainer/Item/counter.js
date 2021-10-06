3d780b7910928eaed7819ee1a5bb12bceec1eddb
Franco <franco.hirschfeldt@mail.com>
2021-10-05 23:18:42 -0300

    styling

--- src/Components/ItemListContainer/Item/counter.js
+++ src/Components/ItemListContainer/Item/counter.js
@@ -1,31 +1,22 @@
 import React from "react";
 import { useState } from "react";
 
-const Counter = ({ initial, stock, productNo }) => {
+const Counter = ({ onAdd, productNo }) => {
   const [counter, setCounter] = useState(0);
-  const [itemStock, setStock] = useState(7);
-  const [cart, setCart] = useState();
+  const [stock, setStock] = useState(7);
+  // const [cart, setCart] = useState({ cant: "" });
 
-  const items = [
-    { key: "1", name: "One", potencia: "200W" },
-    { key: "2", name: "Two", potencia: "300W" },
-    { key: "3", name: "Three", potencia: "400W" },
-    { key: "4", name: "Four", potencia: "800W" },
-    { key: "5", name: "Five", potencia: "1000W" },
-    { key: "6", name: "Six", potencia: "300W" },
-    { key: "7", name: "Three", potencia: "400W" },
-    { key: "8", name: "Four", potencia: "800W" },
-  ];
+  // const { cant } = cart;
 
   function decr() {
     if (counter > 0) {
       setCounter(counter - 1);
-      setStock(itemStock + 1);
+      setStock(stock + 1);
       console.log(`Number ${productNo} stock: ${stock}`);
       console.log(counter);
     } else {
       console.log("ahre");
-      console.log(`stock: ${stock}`);
+      console.log(`Number ${productNo} stock: ${stock}`);
       console.log(counter);
     }
   }
@@ -33,25 +24,24 @@ const Counter = ({ initial, stock, productNo }) => {
   function incr() {
     if (stock > 0) {
       setCounter(counter + 1);
-      setStock(itemStock - 1);
-      console.log(itemStock);
-      console.log(counter);
-    } else if (stock === 0) {
-      setStock(0);
-      console.log(itemStock);
+      setStock(stock - 1);
+      console.log(stock);
       console.log(counter);
     } else {
+      setStock(0);
+      console.log(stock);
+      console.log(counter);
       console.log("ahre no queda nada");
     }
   }
 
-  function addToCart() {
-    let totalCart = items.filter(function (item) {
-      return item.name === productNo;
-    });
-    console.log(totalCart);
-    setCart(totalCart);
-  }
+  // function addToCart() {
+  //   let totalCart = items.filter(function (item) {
+  //     return item.name === productNo;
+  //   });
+  //   console.log(totalCart);
+  //   setCart(totalCart);
+  // }
 
   return (
     <div className="products" id={productNo}>
@@ -61,11 +51,10 @@ const Counter = ({ initial, stock, productNo }) => {
       <div className="name">
         <h2>{productNo}</h2>
       </div>
-      <div>
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" />
-          <span class="input-group-text">.00</span>
-        </div>
+
+      <div id="input" class="input-group mb-3">
+        <input type="text" class="form-control" />
+        <span class="input-group-text">.00</span>
       </div>
 
       <div className="actionButtons">
@@ -89,7 +78,12 @@ const Counter = ({ initial, stock, productNo }) => {
           -
         </button>
 
-        <button onClick={addToCart} type="button" class="btn btn-primary">
+        <button
+          // onClick={() => onAdd(counter)}
+          id="addToCart"
+          type="button"
+          class="btn btn-primary"
+        >
           Add to cart
         </button>
       </div>
--- src/Components/ItemListContainer/itemListContainer.js
+++ src/Components/ItemListContainer/itemListContainer.js
@@ -4,14 +4,14 @@ import Counter from "./Item/counter";
 const ItemListContainer = (props) => {
   return (
     <div className="contenedor">
-      <Counter initial="1" stock="3" productNo="One" />
-      <Counter initial="1" stock="9" productNo="Two" />
-      <Counter initial="1" stock="5" productNo="Three" />
-      <Counter initial="1" stock="10" productNo="Four" />
-      <Counter initial="1" stock="2" productNo="Five" />
-      <Counter initial="1" stock="4" productNo="Six" />
-      <Counter initial="1" stock="6" productNo="Seven" />
-      <Counter initial="1" stock="8" productNo="Eight" />
+      <Counter productNo="One" />
+      <Counter productNo="Two" />
+      <Counter productNo="Three" />
+      <Counter productNo="Four" />
+      <Counter productNo="Five" />
+      <Counter productNo="Six" />
+      <Counter productNo="Seven" />
+      <Counter productNo="Eight" />
     </div>
   );
 };
